c62ecd27f5ccae202bf74ae6cb548543deadf741
liert <2102156704@qq.com>
2026-04-23 20:59:02 +0800

    feat(i18n): enhance Chinese translations

--- translations/zh_CN.ts
+++ translations/zh_CN.ts
@@ -5,7 +5,80 @@
     <name>MainWindow</name>
     <message>
         <source>Send to Rikugan</source>
-        <translation>发送到Rikugan</translation>
+        <translation>发送到 Rikugan</translation>
+    </message>
+    <message>
+        <source>Explain this</source>
+        <translation>解释此内容</translation>
+    </message>
+    <message>
+        <source>Rename with Rikugan</source>
+        <translation>使用 Rikugan 重命名</translation>
+    </message>
+    <message>
+        <source>Deobfuscate with Rikugan</source>
+        <translation>使用 Rikugan 反混淆</translation>
+    </message>
+    <message>
+        <source>Find vulnerabilities</source>
+        <translation>寻找漏洞</translation>
+    </message>
+    <message>
+        <source>Suggest types</source>
+        <translation>建议类型</translation>
+    </message>
+    <message>
+        <source>Annotate function</source>
+        <translation>注释函数</translation>
+    </message>
+    <message>
+        <source>Clean microcode</source>
+        <translation>清理微代码</translation>
+    </message>
+    <message>
+        <source>Xref analysis</source>
+        <translation>交叉引用分析</translation>
+    </message>
+    <message>
+        <source>Send to Bulk Rename</source>
+        <translation>批量重命名</translation>
+    </message>
+
+    <message>
+        <source>Send selection or address to Rikugan input</source>
+        <translation>将选中的内容或地址发送到 Rikugan 输入</translation>
+    </message>
+    <message>
+        <source>Explain the current function with Rikugan</source>
+        <translation>使用 Rikugan 解释当前函数</translation>
+    </message>
+    <message>
+        <source>Analyze and rename the current function</source>
+        <translation>分析并重命名当前函数</translation>
+    </message>
+    <message>
+        <source>Deobfuscate the current function</source>
+        <translation>对当前函数进行反混淆处理</translation>
+    </message>
+    <message>
+        <source>Audit the current function for security bugs</source>
+        <translation>审计当前函数的安全漏洞</translation>
+    </message>
+    <message>
+        <source>Infer and apply types for the current function</source>
+        <translation>推断并应用当前函数的类型</translation>
+    </message>
+    <message>
+        <source>Add comments to the current function</source>
+        <translation>为当前函数添加注释</translation>
+    </message>
+    <message>
+        <source>Clean the microcode for the current function</source>
+        <translation>清理当前函数的微代码 (Microcode)</translation>
+    </message>
+    <message>
+        <source>Deep cross-reference analysis on the current function</source>
+        <translation>对当前函数进行深度的交叉引用分析</translation>
     </message>
 </context>
 </TS>
