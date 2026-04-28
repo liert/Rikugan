0911328f7634eb45d5f393e779b73b38e8b64a80
liert <2102156704@qq.com>
2026-04-28 12:13:15 +0800

    feat: update rendering logic to improve content display and reasoning handling

--- rikugan/ui/message_widgets.py
+++ rikugan/ui/message_widgets.py
@@ -398,13 +398,14 @@ class AssistantMessageWidget(QFrame):
         layout.addWidget(self._content)
 
     def _render(self) -> None:
-        if self._reasoning_text and self._preparing_thought:
+        self._content.setText(md_to_html(self._content_text, self))
+
+        if self._reasoning_text:
             self._thinking_block.show()
-            self._thinking_block.set_thinking(self._reasoning_text, in_progress=True)
+            self._thinking_block.set_thinking(self._reasoning_text, in_progress=self._preparing_thought)
         else:
             self._thinking_block.hide()
 
-        self._content.setText(md_to_html(self._reasoning_text, self))
         self._pending_delta = 0
 
     def append_text(self, delta: str) -> None:
